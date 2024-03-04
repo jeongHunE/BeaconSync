1fcc8068c1583d88dd55d43c74c633178dc8e123
jeungHunLee <ksjs1111@gmail.com>
2024-03-04 18:41:32 +0900

    initial commit

---
+++ SourceCode/BeaconManager.swift
@@ -0,0 +1,8 @@
+//
+//  BeaconManager.swift
+//  BeaconSync
+//
+//  Created by 이정훈 on 3/4/24.
+//
+
+import Foundation
---
+++ SourceCode/BeaconSync.swift
@@ -0,0 +1,8 @@
+//
+//  BeaconSync.swift
+//  BeaconSync
+//
+//  Created by 이정훈 on 3/4/24.
+//
+
+import Foundation
